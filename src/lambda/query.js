97864e135886bf2139661415ded5b77bc906a73d
Mohammed Jan <momo.a.jan@gmail.com>
2019-01-30 11:36:01 +0000

    Refactored poster retrieving code

--- src/lambda/query.js
+++ src/lambda/query.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const JW_BASE_QUERY = 'https://apis.justwatch.com/content/titles/en_GB/popular?body=%7B%22content_types%22:%5B%22show%22,%22movie%22%5D,%22page%22:1,%22page_size%22:12,%22query%22:%22'
-const TMDB_API_KEY = process.env.TMDB_API_KEY 
+const TMDB_API_KEY = '8bf763ff716cf4df79aa2db4f2d9458b' //process.env.TMDB_API_KEY 
 
 const providers = [8,39,9,103,38,137,41,129,99]
 
@@ -13,11 +13,17 @@ function filterOffers(offers) {
 
 async function fetchPosters(item) {
   const { object_type, original_release_year, title } = item
-  const url = `https://api.themoviedb.org/3/search/${object_type === 'movie' ? 'movie' : 'tv'}?api_key=${TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${encodeURI(title)}&${object_type === 'movie' ? 'year' : 'first_air_date_year'}=${original_release_year}`
+  const date_type = object_type === 'movie' ? 'year' : 'first_air_date_year'
+  const format_type = object_type === 'movie' ? 'movie' : 'tv'
+  
+  const TMDB_BASE_URL = 'https://api.themoviedb.org/3/search/'
+  const TMDB_LANG = '&language=en-US'
+  const TMDB_PAGE = '&page=1'
+  const url = `${TMDB_BASE_URL}${format_type}?api_key=${TMDB_API_KEY}${TMDB_LANG}${TMDB_PAGE}&query=${encodeURI(title)}&${date_type}=${original_release_year}`
 
   const resJSON = await fetch(url)
   const response = await resJSON.json()
-  if(response.results[0].poster_path != null) {
+  if(response.results.length > 0 && response.results[0].poster_path) {
     return `https://image.tmdb.org/t/p/w154${response.results[0].poster_path}`
   } else {
     return ''
@@ -48,3 +54,5 @@ export async function handler(event, context) {
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 }
+
+fetchResults('stream').then(res => console.log(res))
